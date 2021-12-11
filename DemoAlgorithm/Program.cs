6a30e6d85b37e2bfcde1cf21a0165e881e224ea1
ost-rv <ost-rv@mail.ru>
2021-12-11 20:55:29 +0300

    Add project files.

---
+++ DemoAlgorithm/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DemoAlgorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var users = GetUsers();
+
+            for (int i = 0; i < users.Length; i++)
+            {
+                Console.WriteLine($"Hi {users[i].Name} ({users[i].Login})");
+
+                if (!users[i].IsPreimium)
+                {
+                    ShowAds();
+                }
+            }
+            Console.ReadKey();
+        }
+
+
+        private static User[] GetUsers()
+        {
+            return new User[] { 
+                new User("Roman", "Роман", false),
+                new User("Andrey", "Андрей", true),
+                new User("Kostey", "Константин", true),
+                new User("Yrko", "Юрий", true),
+            };
+        }
+
+        private static void ShowAds()
+        {
+            Console.WriteLine("Посетите наш новый сайт с бесплатными играми free.games.for.a.fool.com");
+            // Остановка на 1 с
+            Thread.Sleep(1000);
+
+            Console.WriteLine("Купите подписку на МыКомбо и слушайте музыку везде и всегда.");
+            // Остановка на 2 с
+            Thread.Sleep(2000);
+
+            Console.WriteLine("Оформите премиум-подписку на наш сервис, чтобы не видеть рекламу.");
+            // Остановка на 3 с
+            Thread.Sleep(3000);
+        }
+
+    }
+}
---
+++ DemoAlgorithm/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoAlgorithm
+{
+    public class User
+    {
+        public string Login { get; set; }
+        public string Name { get; set; }
+        public bool IsPreimium { get; set; }
+
+        public User(string login, string name, bool isPreimium)
+        {
+            Login = login;
+            Name = name;
+            IsPreimium = isPreimium;
+        }
+
+    }
+}
